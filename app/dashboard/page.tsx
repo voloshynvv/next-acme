e3a59b9bcf8e9a942e8dadf2a59d3c5babad7e8c
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2025-01-03 14:20:10 +0100

    init

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AcmeLogo from '../ui/acme-logo';
+
+const DashboardPage = () => {
+  return <div>dashboard</div>;
+};
+
+export default DashboardPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+
+console.log(inter);
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
